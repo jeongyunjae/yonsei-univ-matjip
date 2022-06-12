60d90727e0de6b1ec0df4006b724c91523f78763
jeongyunjae <yunjae2295@naver.com>
2022-06-12 21:44:59 +0900

    feature: 네이버 크롤링 코드 작성

--- backend-crawling/app.py
+++ backend-crawling/app.py
@@ -1,5 +1,6 @@
 
 import argparse
+from src.naver_crawl import naver_crawl
 from flask import Flask
 
 # 실행 환경변수 설정
@@ -10,7 +11,14 @@ args = parser.parse_args()
 # flask app 생성
 app = Flask(__name__)
 
+def file_creating():
+  file = open('./src/list.csv', 'w', encoding='utf-8')
+  file.write("name" + "|" + "address" + "\n")
+  file.close()
+
 if __name__ == '__main__':
+  file_creating()
+  naver_crawl()
   app.run(host='0.0.0.0', port='3060', debug=True, use_reloader=True)
   
   
